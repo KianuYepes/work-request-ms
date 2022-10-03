a9b0ca188a03f1f7ed28d6248831105c2acd3955
Kianu Yepes <kiayepes69@gmail.com>
2022-10-03 19:10:24 +0800

    Added loginLayout.php, Added login.css, modified the login page

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,77 +1,25 @@
-@extends('layout.main')
-
-@section('title', 'Log In')
+@extends('layout.loginLayout')
 
 @section('content')
 <div class="container">
-    <form action="" method="GET">
-        <h1>Log in as:</h1>
-        <div class="input-box">
-            <button class="open-button" onclick="openRequestorForm()">Requestor</button>
-        </div>
+        
+    <div class="header">
+        <section>Work Request System</section>
+    </div>  
 
-        <div class="input-box">
-            <button class="open-button" onclick="openCaoForm()">CAO</button>
-        </div>
+    <div class="content">
+        <section>
+            <label>Sign In</label>
 
-        <div class="input-box">
-            <button class="open-button" onclick="openStaffForm()">Workstaff</button>
-        </div>
-    </form>
-    
-    {{-- account creation --}}
-    <br>
-    <a href="/user/signup">
-        <div class="create-account">Don't have an Account?</div>
-    </a>
+            <form action='/user/login/admin' method="POST" class="form-container">
+                @csrf
+                <input type="text" name="email" placeholder="Enter Username">
+                <input type="password" name="password" placeholder="Enter Password">
+                <input type="submit" name="submit" value="Login">
+            </form>
+            <p>Don't have an account? <a href="#">Sign Up</a></p>
+        </section>
+    </div>  
 
 </div>
-
-{{-- pop up form --}}
-{{-- requestor --}}
-<div class="form-popup" id="requestorForm">   
-    <form action='/user/login/requestor' method="POST" class="form-container">
-        @csrf
-        <label for="email"><b>Username</b></label>
-        <input type="text" placeholder="Enter username" name="email" required>
-
-        <label for="password"><b>Password</b></label>
-        <input type="password" placeholder="Enter Password" name="password" required>
-
-        <button type="submit" class="btn">Sign In</button>
-        <button type="button" class="btn cancel" onclick="closeRequestorForm()">Close</button>
-    </form>
-</div>
-
-{{-- CAO --}}
-<div class="form-popup" id="caoForm">   
-    <form action='/user/login/admin' method="POST" class="form-container">
-        @csrf
-        <label for="email"><b>Username</b></label>
-        <input type="text" placeholder="Enter username" name="email" required>
-
-        <label for="password"><b>Password</b></label>
-        <input type="password" placeholder="Enter Password" name="password" required>
-
-        <button type="submit" class="btn">Sign In</button>
-        <button type="button" class="btn cancel" onclick="closeCaoForm()">Close</button>
-    </form>
-</div>
-
-{{-- Workstaff--}}
-<div class="form-popup" id="staffForm">   
-    <form action='/user/login/staff' method="POST" class="form-container">
-        @csrf
-        <label for="email"><b>Username</b></label>
-        <input type="text" placeholder="Enter username" name="email" required>
-
-        <label for="password"><b>Password</b></label>
-        <input type="password" placeholder="Enter Password" name="password" required>
-
-        <button type="submit" class="btn">Sign In</button>
-        <button type="button" class="btn cancel" onclick="closeStaffForm()">Close</button>
-    </form>
-</div>
-
-    <h2>This is a Test</h2>
 @endsection
