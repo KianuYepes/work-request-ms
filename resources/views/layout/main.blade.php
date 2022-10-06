5cf8be49af58b7c9afaedf9ac602ac1e44487d74
Kianu Yepes <kiayepes69@gmail.com>
2022-10-06 14:16:35 +0800

    Changed login-page

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -5,9 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
-   <!--  <link rel="stylesheet" href={{ asset('assets/css/styles.css') }}> -->
+    <!-- <link rel="stylesheet" href={{ asset('assets/css/styles.css') }}> -->
     <link rel="stylesheet" type="text/css" href={{ asset('assets/css/login.css') }}>
-    <script src={{ asset('assets/js/test.js') }} defer></script>
+    <!-- <script src={{ asset('assets/js/test.js') }} defer></script> -->
 </head>
 <body>
     @yield('content')
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -9,15 +9,46 @@
 
     <div class="content">
         <section>
-            <label>Sign In</label>
+            <label class="title">Sign In</label>
+            <!-- container for slider -->
+            <div class="wrapper" id="slide">
+                
+                    <span class="form1">
+                        <!-- === Requestor === -->
+                        <form action='/user/login/requestor' method="POST" class="form-container">
+                            @csrf
+                            <label>As Requestor</label>
+                            <input type="text" name="email" placeholder="Enter Username">
+                            <input type="password" name="password" placeholder="Enter Password">
+                            <input type="submit" name="submit" value="Login">
+                        </form>
+                    </span>
 
-            <form action='/user/login/admin' method="POST" class="form-container">
-                @csrf
-                <input type="text" name="email" placeholder="Enter Username">
-                <input type="password" name="password" placeholder="Enter Password">
-                <input type="submit" name="submit" value="Login">
-            </form>
-            <p>Don't have an account? <a href="#">Sign Up</a></p>
+                    <span class="form2">
+                        <!-- === Wrok Staff === -->
+                        <form action='/user/login/staff' method="POST" class="form-container">
+                            @csrf
+                            <label>As Work Staff</label>
+                            <input type="text" name="email" placeholder="Enter Username">
+                            <input type="password" name="password" placeholder="Enter Password">
+                            <input type="submit" name="submit" value="Login">
+                        </form>
+                    </span>
+
+                    <span class="form3">
+                        <!-- === Admin === -->
+                        <form action='/user/login/admin' method="POST" class="form-container">
+                            @csrf
+                            <label>As Administrator</label>
+                            <input type="text" name="email" placeholder="Enter Username">
+                            <input type="password" name="password" placeholder="Enter Password">
+                            <input type="submit" name="submit" value="Login">
+                        </form>
+                    </span>
+                
+            </div><!-- END container for slider -->
+            
+            <p>Don't have an account? <a href="/user/signup">Sign Up</a></p>
         </section>
     </div>  
 
